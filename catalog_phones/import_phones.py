f1e4faada0a2526179a53e54fed54fcafc8014a2
Виталий Чухляев <vitaliy69@mail.ru>
2023-04-03 11:02:46 +0300

    Created file import_phones.py

---
+++ catalog_phones/import_phones.py
@@ -0,0 +1,23 @@
+import csv
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
+
+from catalog_phones.models import Phone
+
+
+class ImportPhoneCommand(BaseCommand):
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for row in reader:
+                phone = Phone()
+                phone.name = row['name']
+                phone.price = float(row['price'])
+                phone.image = row['image']
+                phone.release_date = datetime.strptime(row['release_date'], '%Y-%m-%d')
+                phone.lte_exists = row['lte_exists'].lower() == 'true'
+                phone.slug = slugify(phone.name)
+                phone.save()
